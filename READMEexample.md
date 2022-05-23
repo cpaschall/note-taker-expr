7ef7b9448e71f6c210cf07b0bfd553eecf559a4b
cpaschall <cpaschall216@gmail.com>
2022-05-23 14:07:09 -0400

    refactoring server.js

---
+++ READMEexample.md
@@ -0,0 +1,38 @@
+
+  ![License: Apache 2.0](https://img.shields.io/badge/License-Apache_2.0-blue.svg)
+
+  # Note Taker
+
+  ## Description
+  User input title and body of a note which displays to left of the screen.  User can then delete notes. This project showcases my back-end programming abilities by creating a server.  Project is linked to a heroku server
+
+  ## Instructions
+  Install the following packages in the terminal:
+  ```
+  npm init -y
+  npm i inquirer
+  npm i express
+  ```
+
+  ## Usage
+   Navigate to the directory containing the application within a terminal.  Make sure the necessary modules have been installed.  Excute the following command:
+  ```
+  node server
+  ```
+
+  ## Contributing
+  Fork the repo then contact me to request to be added to the project if you would like to contribute.
+
+  ## Tests
+  N/A
+
+  ## Questions
+  Github: [cpaschall](https://github.com/cpaschall)
+  <br>
+  Email: cpaschall216@gmail.com
+
+  ---
+
+  [License: Apache 2.0](https://opensource.org/licenses/Apache-2.0)
+ 
+  
--- server.js
+++ server.js
@@ -1,30 +1,24 @@
-// GIVEN a note-taking application
-
 const express = require('express');
 const path = require('path');
-const uuid = require('./uuid');
-const db = require('./Develop/db/db.json')
+const uuid = require('./helpers/uuid');
+const db = require('./db.json')
 const fs = require('fs');
 const { readFromFile, writeToFile } = require('../../../upenn-fs-repo/01-Activities/11-Express/01-Activities/28-Stu_Mini-Project/Main/helpers/fsUtils');
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
+
 // Middleware for urlecoded data
-// `urlencoded` data represents a URL encoded form. If we had a HTML form with fields: `username` and `password`, our urlencoded data would be "username=JohnAppleseed&password=passw0rd"
-// This middleware will parse that string into an object containing key value pairs
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-
-
-// WHEN I click on the link to the notes page
-// THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
+// route to notes.html
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
+// route to notes database (db.json)
 app.get('/api/notes', (req, res) => {
   return res.status(200).json(db)
 });
@@ -60,17 +54,7 @@ app.post('/api/notes', (req, res) => {
   }
 });
 
-// const { title, text } = 
-
-// WHEN I enter a new note title and the note’s text
-// THEN a Save icon appears in the navigation at the top of the page
-// WHEN I click on the Save icon
-// THEN the new note I have entered is saved and appears in the left-hand column with the other existing notesers\cpasc\OneDrive\UPENN\Homework\week-12\not
-// WHEN I click on an existing note in the list in the left-hand column
-// THEN that note appears in the right-hand column
-// WHEN I click on the Write icon in the navigation at the top of the page
-// THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
-
+//  deletes a note from the notes list
 app.delete('/api/notes/:id', (req, res) => {
   const noteId = req.params.id;
   readFromFile('./Develop/db/db.json')
@@ -81,10 +65,11 @@ app.delete('/api/notes/:id', (req, res) => {
       res.json(`Note ${noteId} has been deleted`)
     });
 })
-// WHEN I open the Note Taker
-// THEN I am presented with a landing page with a link to a notes page
+
+// routes for all other  GET requests go to index.html page
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+//  start server on indacted port #
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
