1bdcb3ec6d78f0e17bc96e99bf695c3578e5fa37
cpaschall <cpaschall216@gmail.com>
2022-05-23 22:05:04 -0400

    removing Develop folder - heroku troubleshooting

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
-const uuid = require('./Develop/helpers/uuid');
-const db = require('./Develop/db/db.json')
+const uuid = require('./helpers/uuid');
+const db = require('./db/db.json')
 const fs = require('fs');
 
 const PORT = process.env.PORT || 3001;
@@ -10,11 +10,11 @@ const app = express();
 // Middleware for urlecoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('Develop/public'));
+app.use(express.static('public'));
 
 // route to notes.html
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'Develop/public/notes.html'));
+  res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
 // route to notes database (db.json)
@@ -32,13 +32,13 @@ app.post('/api/notes', (req, res) => {
       id : uuid(),
     };
     // get existing notes
-    fs.readFile('./Develop/db/db.json', 'utf8', (err, data) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
       if(err) {
         console.error(err);
       } else {
         const parsedNotes =JSON.parse(data);
         parsedNotes.push(newNote);
-        fs.writeFile('./Develop/db/db.json', JSON.stringify(parsedNotes, null, 4), (writeErr) => 
+        fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null, 4), (writeErr) => 
           writeErr ? console.error(writeErr) : console.info("Successfully added note to database.")
         );
       }
@@ -56,18 +56,18 @@ app.post('/api/notes', (req, res) => {
 //  deletes a note from the notes list
 app.delete('/api/notes/:id', (req, res) => {
   const noteId = req.params.id;
-  fs.readFile('./Develop/db/db.json')
+  fs.readFile('./db/db.json')
     .then((data) => JSON.parse(data))
     .then((json) => {
       deletedNote = json.filter((note) => note.id !== noteId);
-      fs.writeFile('./Develop/db/db.json', deletedNote);
+      fs.writeFile('./db/db.json', deletedNote);
       res.json(`Note ${noteId} has been deleted`)
     });
 })
 
 // routes for all other  GET requests go to index.html page
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'Develop/public/index.html'))
+  res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
 //  start server on indacted port #
